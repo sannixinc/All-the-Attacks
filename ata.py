4d04a6f974145c4e8af0f7a59fdac04ba66c90e2
root <root@localhost.localdomain>
2019-08-10 21:53:01 -0400

    added technique and mitigation lookup

--- ata.py
+++ ata.py
@@ -22,14 +22,40 @@ def tools():
             file.write("Tool name: {}\n".format(tool.name))
                 
             for actor in tool.actors:
-                print("ACtor Alias: {}".format(actor.aliases))
-                file.write("Actor Alias: {}\n".format(actor.aliases))
-                
-                #for malware in actor.malware:
-                    #print("Malware: {}".format(malware.name))
+                print("Actor name: {}".format(actor.name))
+                file.write("Actor name: {}\n".format(actor.name))
                 
     file.close()
 
-
+def actors():
+    file = open("ata.txt", "a")
+    act = input( "\nActor name to lookup: ")
+    
+    file.write("\nActor Name: {}\n".format (act))
+    for actor in attack.actors:
+         if act in actor.name:
+            for technique in actor.techniques:
+                print(technique.name)
+                file.write("{}\n".format(technique.name))
+         else:
+             continue
+    file.close()
+         
+def technique():
+    file = open("ata.txt", "a")
+    mit = input("\n Technique to lookup: ")
+    
+    file.write("\n Technique Name:{}\n".format(mit))
+    for technique in attack.techniques:
+        if mit in technique.name:
+            for mitigation in technique.mitigation:
+                print(mitigation.description)
+                file.write("{}\n".format(mitigation.description))
+        else:
+            continue
+    file.close()
+         
 if __name__=="__main__":
     tools()
+    actors()
+    technique()
